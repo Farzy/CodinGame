eb97f873fddb5aa358c153f75a724c66a9e619c5
Farzad FARID <farzy@farzy.org>
2020-06-21 14:25:40 +0200

    Rename Rust solution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+mod the_descent;
+mod onboarding;
+mod power_of_thor_episode_1;
+mod test;
+
+use std::io;
+
+macro_rules! parse_input {
+    ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
+}
+
 fn main() {
-    println!("Hello, world!");
 }
